bdc4801f784211c88fb920f5bbaf8502a5d18fd6
vspaz <vladimir.edelman@gmail.com>
2025-01-20 00:16:25 +0100

    add wsl & test stubs

---
+++ src/wls.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const point = @import("point.zig");
+
+pub const Wls = struct {
+    x_points: *std.ArrayList(f64),
+    y_points: *std.ArrayList(f64),
+    weights: *std.ArrayList(f64),
+
+    pub fn fit_linear_regression(self: Wls) point.Point {
+        return point.Point{ .intercept = self.x_points.items[0], .slope = self.x_points.items[0] };
+    }
+};
+
+test "test Wls ok" {
+    const allocator = std.heap.general_purpose_allocator;
+    var x_points = std.ArrayList(f64).init(allocator);
+    defer x_points.deinit();
+    try x_points.append(1);
+    try x_points.append(0.9);
+    const wls = Wls{ .x_points = &x_points, .y_points = &x_points, .weights = &x_points };
+    try std.testing.expectEqual(@as(f64, 1), wls.fit_linear_regression().get_slope());
+}
