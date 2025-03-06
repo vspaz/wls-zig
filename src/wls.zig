2b2a03ac10a3165ebf1f994dfb6f170075af6288
vspaz <vladimir.edelman@gmail.com>
2025-03-07 00:18:42 +0100

    style fixes

--- src/wls.zig
+++ src/wls.zig
@@ -2,19 +2,7 @@ const std = @import("std");
 pub const Wls = @import("model.zig").Wls;
 pub const asserts = @import("asserts.zig");
 
-pub fn main() !void {
-    const x_points = [_]f64{ 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0 };
-    const y_points = [_]f64{ 1.0, 3.0, 4.0, 5.0, 2.0, 3.0, 4.0 };
-    const weights = [_]f64{ 1.0, 2.0, 3.0, 1.0, 8.0, 1.0, 5.0 };
-
-    const wls_model = Wls.init(&x_points, &y_points, &weights);
-    const fitted_model = wls_model.fit_linear_regression();
-
-    if (fitted_model) |line| {
-        asserts.assert_almost_equal(2.14285714, line.intercept, 1.0e-6);
-        asserts.assert_almost_equal(0.150862, line.slope, 1.0e-6);
-    }
-}
+pub fn main() !void {}
 
 test {
     std.testing.refAllDecls(@This());
